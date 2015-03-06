275f50f2320215ddf676b6910d6c7880b5ed821b
Roman <Roman@192.168.1.101>
2015-03-06 15:26:59 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+// for use in development mode*****
+error_reporting(E_ALL);
+
+define('BASE_PATH', dirname(realpath(__FILE__)));
+//define('APP_PATH', 'BASE_PATH'/app)
+?>
+
+
+<hr>
+<a class="n" target="_top" onclick="h(this);" href="/phpmyadmin/">phpMyAdmin</a>
+
+
+
+
